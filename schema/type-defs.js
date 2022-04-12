801d8f51d2537f1fc1495664f301f7f838e28d71
Gabriel Ayala <gabeayala100@gmail.com>
2022-04-12 13:02:45 -0700

    Create a folder for the data types and the functions,resolvers, and the dummy data.

---
+++ schema/type-defs.js
@@ -0,0 +1,15 @@
+const { gql } = require("apollo-server"); //this allows graphql language to be used so that javascript can understand it
+
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    username: String!
+    age: Int!
+    nationality: String!
+  }
+  type Query {
+    users: [User!]!
+  }
+`;
