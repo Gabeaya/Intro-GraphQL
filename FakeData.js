f3de14dbef42526d2fe268a459d095caf184067a
Gabriel Ayala <gabeayala100@gmail.com>
2022-04-12 13:23:10 -0700

    Add some mock data but move it out of the schema folder.

---
+++ FakeData.js
@@ -0,0 +1,32 @@
+const UserList = [
+  {
+    id: 1,
+    name: "john",
+    username: "JOJOO",
+    age: 20,
+    nationality: "Canada",
+  },
+  {
+    id: 2,
+    name: "Pedro",
+    username: "Pedi",
+    age: 21,
+    nationality: "Brazil",
+  }, 
+  {
+    id: 3,
+    name: "darrell",
+    username: "dman",
+    age: 33,
+    nationality: "New York",
+  },
+  {
+    id: 4,
+    name: "Yammen",
+    username: "JOInsJOO",
+    age: 19,
+    nationality: "Pakistan",
+  },
+]
+
+module.exports = { UserList};
