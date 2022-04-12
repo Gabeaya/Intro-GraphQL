a88f7c995a9d9aa367638f5e6b5ea3980dfdde1c
Gabriel Ayala <gabeayala100@gmail.com>
2022-04-12 13:03:22 -0700

    Add a function that will notify, through the console, whether or not the server is running.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const {ApolloServer} = require("apollo-server");
 
 const server = new ApolloServer({typeDefs, resolvers}); // typeDefs are the data and queries that are defined. functions that make calls are known as resolvers
+
+server.listen().then(() => {
+  console.log(`Your API IS RUNNING AT: ${url} :)`);
+})
