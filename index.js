8d1f4b3130ee5661403c305bd9098dd7a3231690
Gabriel Ayala <gabeayala100@gmail.com>
2022-04-12 13:23:57 -0700

    Import the resolvers funcrtion into the index file.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const {ApolloServer} = require("apollo-server");
-const { typeDefs } = require("./schema/type-defs")
-
+const { typeDefs } = require("./schema/type-defs");
+const { resolvers } = require("./schema/resolvers");
 const server = new ApolloServer({typeDefs, resolvers}); // typeDefs are the data and queries that are defined. functions that make calls are known as resolvers
 
-server.listen().then(() => {
+server.listen().then(({url}) => {
   console.log(`Your API IS RUNNING AT: ${url} :)`);
 })
