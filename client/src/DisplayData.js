441d00ca3010a6c0b5463c7893ff2fe7c4a0bb52
Gabriel Ayala <gabeayala100@gmail.com>
2022-04-14 11:50:24 -0700

    define the queries, create laoding and error conditionals, and return the data.

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useQuery, gql} from "@apollo.client";
+import {useQuery, gql} from "@apollo/client";
 
 const QUERY_ALL_USERS = gql`
   query GetAllUsers {
@@ -8,14 +8,47 @@ const QUERY_ALL_USERS = gql`
       name
       age
       username
+      nationality
       }
   }
-`
+`;
+
+const QUERY_ALL_MOVIES = gql`
+  query GetAllMovies {
+    movies {
+      name
+      twoThumbsUp
+    }
+  }
+`;
+
 function DisplayData() {
-  const {} = useQuery(QUERY_ALL_USERS);
+  const {data, loading, error} = useQuery(QUERY_ALL_USERS);
+  const {data: movieData } = useQuery(QUERY_ALL_MOVIES);
+  if (loading) {
+    <h1>Data is Loading...</h1>
+  }
+  else if (error) {
+    <h1> {error} </h1>
+  }
   return (
-    <div>User List</div>
-  )
+    <div>
+      {data && 
+        data.users.map((user) => {
+          return (
+            <div>
+              <h1>Name: {user.nameUser}</h1>
+              <h1>UserName: {user.username}</h1>
+              <h1>Age: {user.age}</h1>
+              <h1>Nationality: {user.nationality}</h1>
+            </div>
+          );
+        })}
+        {movieData && movieData.movies.map((movie) => {
+          return <h1>Movie Name: {movie.name}</h1>
+        })}
+    </div>
+  );
 }
 
 export default DisplayData
